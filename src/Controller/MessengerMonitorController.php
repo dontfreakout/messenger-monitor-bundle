4ab257653c98a3442cec04f61c3b3e699e1c8ea4
kbond <kbond@users.noreply.github.com>
2024-11-12 03:52:30 +0000

    bot: fix cs [skip ci]

--- src/Controller/MessengerMonitorController.php
+++ src/Controller/MessengerMonitorController.php
@@ -15,7 +15,6 @@ use Lorisleiva\CronTranslator\CronTranslator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Messenger\Message\RedispatchMessage;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\SentToFailureTransportStamp;
